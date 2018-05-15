b1d1b1ddaaca2a0158715b40e06c4c5dde39c212
Mikolaj Kozakiewicz <smmiki@gmail.com>
2018-05-15 17:49:17 +0200

    imagenet

---
+++ imagenet/server.py
@@ -0,0 +1,63 @@
+import os
+from flask import Flask, render_template, request
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+
+from classify_image import run_inference_on_image
+
+app = Flask(__name__)
+
+photos = UploadSet('photos', IMAGES)
+
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/img'
+configure_uploads(app, photos)
+
+currentfilename = ""
+
+fileTypes = {
+    "plastic": [
+        'water bottle',
+        'bottle',
+        'water jug'
+    ],
+    "glass": [
+        'goblet',
+        'beer glass',
+        'cocktail shaker',
+    ],
+    "paper": [
+        "envelope",
+        "paper towel",
+        "carton",
+    ]
+}
+
+def cleanup():
+    # os.remove(app.config['UPLOADED_PHOTOS_DEST'] + "/" + currentfilename)
+    os.remove('logger.txt')
+
+def find_el_in_log():
+    found_element = 'no found elements'
+    with open('logger.txt') as f:
+        suggestion = f.readline()
+        suggestion2 = f.readline()
+        suggestion3 = f.readline()
+
+    for filetype in fileTypes:
+        for element in fileTypes[filetype]:
+            if element in suggestion or element in suggestion2 or element in suggestion3:
+                found_element = filetype
+                cleanup()
+                return filetype
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        print("FILENAME ******** /n/n/n", request.files['photo'])
+        currentfilename = photos.save(request.files['photo'])
+        print('here')
+        os.system("python classify_image.py --image_file >> logger.txt {}".format(app.config['UPLOADED_PHOTOS_DEST'] + "/" + currentfilename))
+
+        return find_el_in_log()
+
+if __name__ == '__main__':
+    app.run(debug=True)
